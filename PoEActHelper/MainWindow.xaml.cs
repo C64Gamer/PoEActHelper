b47e437f59363fc90da86ab8094524634bb08457
C64Gamer <info@c64gamer.com>
2025-10-26 17:03:07 +0100

    Adding resize functionallity to the main window

--- PoEActHelper/MainWindow.xaml.cs
+++ PoEActHelper/MainWindow.xaml.cs
@@ -16,14 +16,49 @@ namespace PoEActHelper
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const double AspectRatio = 800.0 / 450.0;
+        private bool isResizing = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.SizeChanged += MainWindow_SizeChanged;
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (isResizing) return;
+
+            isResizing = true;
+
+            if (e.WidthChanged)
+            {
+                this.Height = this.Width / AspectRatio;
+            }
+            else if (e.HeightChanged)
+            {
+                this.Width = this.Height * AspectRatio;
+            }
+
+            isResizing = false;
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
         }
+
+        private void ResizeGrip_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            this.ResizeMode = ResizeMode.CanResize;
+            NativeMethods.SendMessage(new System.Windows.Interop.WindowInteropHelper(this).Handle,
+                0x112, (IntPtr)61448, IntPtr.Zero);
+        }
+    }
+    internal static class NativeMethods
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        internal static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
     }
 }
