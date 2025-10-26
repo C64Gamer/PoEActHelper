c64c6ad8262248fb89a2d1e80b49a12cad1c818b
C64Gamer <info@c64gamer.com>
2025-10-26 15:10:03 +0100

    Adding drag & drop for the main window + background transparency

--- PoEActHelper/MainWindow.xaml.cs
+++ PoEActHelper/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace PoEActHelper
         {
             InitializeComponent();
         }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
     }
 }
